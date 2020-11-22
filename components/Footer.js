437daa453414ae6fd6d37f269200e92eba5a22ef
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-22 22:31:55 +0000

    Create personal Navbar and Footer

---
+++ components/Footer.js
@@ -0,0 +1,59 @@
+// Based on <root>/components/CreativeTim/Footer/Footer.js
+/*eslint-disable*/
+import React from "react";
+import PropTypes from "prop-types";
+// @material-ui/core components/CreativeTim
+import { makeStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+// core components/CreativeTim
+import styles from "assets/CreativeTim/jss/nextjs-material-dashboard/components/footerStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Footer(props) {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a href="https://github.com/stefanjcollier/DnD_DM_Sidekick" className={classes.block}>
+                Client GH
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="https://github.com/stefanjcollier/DnD_DM_Sidekick_API" className={classes.block}>
+                Backend GH
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="" className={classes.block}>
+                Demo
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#blog" className={classes.block}>
+                Blog
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <p className={classes.right}>
+          <span>
+            &copy; {1900 + new Date().getYear()}{" "}
+            <a
+              href="https://www.creative-tim.com?ref=njsmd-footer"
+              target="_blank"
+              className={classes.a}
+            >
+              Creative Tim
+            </a>
+            , made with love for a better web
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+}
