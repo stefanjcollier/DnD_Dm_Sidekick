14977ac0cb8c16e9ae7a4f52267ab12327b5c76b
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-22 22:31:38 +0000

    Create personal Layout
    based on CT/Admin.js

---
+++ layouts/Layout.js
@@ -0,0 +1,104 @@
+// Stefan here, Based on <root>/layouts/CreativeTim/Admin.js
+import React from "react";
+import { useRouter } from "next/router";
+// creates a beautiful scrollbar
+import PerfectScrollbar from "perfect-scrollbar";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+// @material-ui/core components/CreativeTim
+import { makeStyles } from "@material-ui/core/styles";
+// core components/CreativeTim
+import Navbar from "components/CreativeTim/Navbars/Navbar.js";
+import Sidebar from "components/CreativeTim/Sidebar/Sidebar.js";
+
+// core components
+import Footer from "components/Footer.js";
+
+import routes from "routes.js";
+
+import styles from "assets/CreativeTim/jss/nextjs-material-dashboard/layouts/adminStyle.js";
+
+import bgImage from "assets/CreativeTim/img/sidebar-2.jpg";
+import logo from "assets/CreativeTim/img/reactlogo.png";
+
+let ps;
+
+const useStyles = makeStyles(styles);
+
+export default function Admin({ children, ...rest }) {
+  // used for checking current route
+  const router = useRouter();
+  // styles
+  const classes = useStyles();
+  // ref to help us initialize PerfectScrollbar on windows devices
+  const mainPanel = React.createRef();
+  // states and functions
+  const [image, setImage] = React.useState(bgImage);
+  const [color, setColor] = React.useState("white");
+  const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleImageClick = (image) => {
+    setImage(image);
+  };
+  const handleColorClick = (color) => {
+    setColor(color);
+  };
+  const handleFixedClick = () => {
+    if (fixedClasses === "dropdown") {
+      setFixedClasses("dropdown show");
+    } else {
+      setFixedClasses("dropdown");
+    }
+  };
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const resizeFunction = () => {
+    if (window.innerWidth >= 960) {
+      setMobileOpen(false);
+    }
+  };
+  // initialize and destroy the PerfectScrollbar plugin
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(mainPanel.current, {
+        suppressScrollX: true,
+        suppressScrollY: false,
+      });
+      document.body.style.overflow = "hidden";
+    }
+    window.addEventListener("resize", resizeFunction);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+      }
+      window.removeEventListener("resize", resizeFunction);
+    };
+  }, [mainPanel]);
+  return (
+    <div className={classes.wrapper}>
+      {/*<Sidebar*/}
+      {/*  routes={routes}*/}
+      {/*  logoText={"Creative Tim"}*/}
+      {/*  logo={logo}*/}
+      {/*  image={image}*/}
+      {/*  handleDrawerToggle={handleDrawerToggle}*/}
+      {/*  open={mobileOpen}*/}
+      {/*  color={color}*/}
+      {/*  {...rest}*/}
+      {/*/>*/}
+      <div className={classes.mainPanel} ref={mainPanel}>
+        <Navbar
+          routes={routes}
+          handleDrawerToggle={handleDrawerToggle}
+          {...rest}
+        />
+        {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
+        <div className={classes.content}>
+          <div className={classes.container}>{children}</div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
