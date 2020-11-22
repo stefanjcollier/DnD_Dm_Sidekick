4c5f9ee615c03175b132d68910d94388cdcbe112
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-22 21:55:39 +0000

    integrate creative tim Admin layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import 'assets/CreativeTim/css/nextjs-material-dashboard.css';
+import React from 'react';
+import Head from 'next/head';
+import Admin from 'layouts/CreativeTim/Admin';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+        <title>D&D DM Sidekick</title>
+      </Head>
+      <Admin>
+        <Component {...pageProps} />
+      </Admin>
+    </React.Fragment>
+  )
 }
 
 export default MyApp
