d4431acb4330e23b212774876ce2c6e81d4a4fab
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-22 23:02:55 +0000

    sort out header items

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import { useRouter } from "next/router";
-// @material-ui/core components/CreativeTim
+// @material-ui/components
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -10,21 +10,24 @@ import IconButton from "@material-ui/core/IconButton";
 import Hidden from "@material-ui/core/Hidden";
 // @material-ui/icons
 import Menu from "@material-ui/icons/Menu";
-// core components/CreativeTim
-import AdminNavbarLinks from "./AdminNavbarLinks.js";
+// core CreativeTim components
 import Button from "components/CreativeTim/CustomButtons/Button.js";
+// core components
+import HeaderLinks from "./HeaderLinks.js";
 
 import styles from "assets/CreativeTim/jss/nextjs-material-dashboard/components/headerStyle.js";
-
 const useStyles = makeStyles(styles);
 
+
+const defaultPageTitle = "D&D Dungeon Master's Sidekick";
+
 export default function Header(props) {
-  // used for checking current route
-  const router = useRouter();
   // create styles for this component
   const classes = useStyles();
-  function makeBrand() {
-    var name = "D&D Dungeon Master's Sidekick";
+  // used for checking current route
+  const router = useRouter();
+  function makePageTitle() {
+    var name = defaultPageTitle;
     props.routes.map((prop) => {
       if (router.route.indexOf(prop.layout + prop.path) !== -1) {
         name = prop.name;
@@ -43,11 +46,11 @@ export default function Header(props) {
         <div className={classes.flex}>
           {/* Here we create navbar brand, based on route name */}
           <Button color="transparent" href="#" className={classes.title}>
-            {makeBrand()}
+            {makePageTitle()}
           </Button>
         </div>
         <Hidden smDown implementation="css">
-          <AdminNavbarLinks />
+          <HeaderLinks />
         </Hidden>
         <Hidden mdUp implementation="css">
           <IconButton
