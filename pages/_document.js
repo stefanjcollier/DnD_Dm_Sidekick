d1987c33f40a40788dca541cd0ac76c3a53bf8d7
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-23 00:12:12 +0000

    Use React.HTML instead of basic html tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/styles";
 
 class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
@@ -39,7 +39,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
