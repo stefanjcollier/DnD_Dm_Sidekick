1cf08ea8d4a2f3be1d42aaa76642b9a3658c6879
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-23 16:21:45 +0000

    replace favicon. with D20

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href={require("assets/CreativeTim/img/favicon.png")} />
+          <link rel="shortcut icon" href={require("assets/img/favicon.png")} />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
