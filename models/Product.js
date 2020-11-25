05b19a13e0ac793534b5f3fee8527bfa4254a78a
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-25 21:37:32 +0000

    include Product id from API

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,15 @@
 import Price from "models/Price"
 
 export default class Product {
-  constructor (name, weight, gold, silver, copper) {
+  constructor (id, name, weight, gold, silver, copper) {
+    this.id = id
     this.name = name
     this.weight = weight
     this.price = new Price(gold, silver, copper)
   }
 
   static fromObject(obj){
-    return new Product(obj.name, obj.weight, obj.gp_price, obj.sp_price, 0)
+    return new Product(obj.id, obj.name, obj.weight, obj.gp_price, obj.sp_price, 0)
   }
   static fromObjects(array){
     return array.map((rawProduct) => Product.fromObject(rawProduct))
