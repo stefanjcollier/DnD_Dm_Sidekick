c7bf94733235883894a617ff60a6db07a41ffe42
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-25 00:26:31 +0000

    move Basket logic into Products

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from 'react';
 import {makeStyles, withStyles} from "@material-ui/core/styles";
 // core CreativeTim components
 import Table from "components/CreativeTim/Table/Table.js";
+import Button from "components/CreativeTim/CustomButtons/Button.js";
 import Card from "components/CreativeTim/Card/Card.js";
 import CardHeader from "components/CreativeTim/Card/CardHeader.js";
 import CardBody from "components/CreativeTim/Card/CardBody.js";
+import GridContainer from "../CreativeTim/Grid/GridContainer";
+import GridItem from "../CreativeTim/Grid/GridItem";
 // Axios
 import axios from "axios";
+// Icons
+import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
+//
+import Basket from "components/Products/Basket";
 
 import styles from "assets/jss/products.js";
 const useStyles = makeStyles(styles);
@@ -39,6 +46,12 @@ export default function Products(props) {
           productObj.name,
           productObj.price_str,
           productObj.weight.toString(),
+          <Button
+            color='info'
+            onClick={() => { alert(productObj.name)}}
+          >
+            <AddShoppingCart/>
+          </Button>
         ];
       }
     )
@@ -51,17 +64,24 @@ export default function Products(props) {
 
   const classes = useStyles();
   return (
-    <Card>
-      <CardHeader color="primary">
-        <h4 className={classes.cardTitleWhite}>Products</h4>
-      </CardHeader>
-      <CardBody>
-        <Table
-          tableHeaderColor="primary"
-          tableHead={["Name", "Base Cost", "Weight (lbs)"]}
-          tableData={productList()}
-        />
-      </CardBody>
-    </Card>
+    <GridContainer>
+      <GridItem xs={9} sm={9} md={9}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Products</h4>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["Name", "Base Cost", "Weight (lbs)", '']}
+              tableData={productList()}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+      <GridItem xs={3} sm={3} md={3}>
+        <Basket />
+      </GridItem>
+    </GridContainer>
   )
 }
--- pages/shop.js
+++ pages/shop.js
@@ -9,6 +9,7 @@ import CardHeader from "components/CreativeTim/Card/CardHeader.js";
 import CardBody from "components/CreativeTim/Card/CardBody.js";
 // components
 import Products from "components/Products/Products";
+import Basket from "components/Products/Basket";
 
 import styles from "assets/jss/products.js";
 const useStyles = makeStyles(styles);
@@ -16,35 +17,5 @@ const useStyles = makeStyles(styles);
 export default function Home() {
   const classes = useStyles();
 
-  return(
-    <GridContainer>
-      <GridItem xs={9} sm={9} md={9}>
-        <Products />
-      </GridItem>
-      <GridItem xs={3} sm={3} md={3}>
-        <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["Name", "Country", "City", "Salary"]}
-              tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"],
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
-  );
+  return(<Products />);
 }
