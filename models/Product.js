94fd891e1827661737404b129b1396d2178987fd
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-28 12:35:45 +0000

    make Product an ApiModel

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,20 @@
+import ApiModel from "models/Abstract/ApiModel";
 import Price from "models/Price"
 
-export default class Product {
+export default class Product extends ApiModel{
   constructor (id, name, weight, gold, silver, copper) {
-    this.id = id
+    super(id)
     this.name = name
     this.weight = weight
     this.price = new Price(gold, silver, copper)
   }
 
+  static endpoint() {
+    return 'api/products/'
+  }
+
   static fromObject(obj){
     return new Product(obj.id, obj.name, obj.weight, obj.gp_price, obj.sp_price, 0)
-  }
-  static fromObjects(array){
-    return array.map((rawProduct) => Product.fromObject(rawProduct))
   }
 
   get price_str() {
