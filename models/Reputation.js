17fa864d2d995260f47765d79db5ecbbdca29871
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-28 11:13:50 +0000

    Create reputation model

---
+++ models/Reputation.js
@@ -0,0 +1,12 @@
+export default class Reputation {
+  constructor(id, name, description) {
+    this.id  = id
+    this.name = name
+    this.description = description
+  }
+
+  static fromObject(obj){
+    return new Reputation(obj.id, obj.name, obj.description)
+  }
+
+}
