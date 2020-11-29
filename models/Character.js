958c6467705b7177d75a96ca7b25bdd566f35726
Stefan Collier <stefanjcollier@hotmail.com>
2020-11-29 10:47:34 +0000

    add images to characters

--- models/Character.js
+++ models/Character.js
@@ -2,11 +2,12 @@ import ApiModel from "models/Abstract/ApiModel"
 import Reputation from "models/Reputation";
 
 export default class Character extends ApiModel {
-  constructor (id, name, charisma_modifier, reputation) {
+  constructor (id, name, charisma_modifier, reputation, imageUrl) {
     super(id);
     this.name = name
     this.charisma_modifier = charisma_modifier
     this.reputation = reputation
+    this.imageUrl = imageUrl
   }
 
   static endpoint(){
@@ -15,7 +16,7 @@ export default class Character extends ApiModel {
 
   static fromObject(obj) {
     const reputation = Reputation.fromObject(obj.reputation)
-    return new Character(obj.id, obj.name, obj.charisma_modifier, reputation)
+    return new Character(obj.id, obj.name, obj.charisma_modifier, reputation, obj.remoteImageUrl)
   }
 
 }
