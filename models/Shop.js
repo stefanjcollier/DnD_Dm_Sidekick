717729f11676e48e052beb0dd1a680b05cb6d8a3
Stefan Collier <stefanjcollier@hotmail.com>
2020-12-07 22:04:34 +0000

    Add shop entity

---
+++ models/Shop.js
@@ -0,0 +1,21 @@
+import ApiModel from "models/Abstract/ApiModel";
+import Product from "models/Product"
+
+export default class Shop extends ApiModel{
+  constructor(id, name, description, products) {
+    super(id)
+    this.name = name
+    this.description = description
+    this.products = products
+  }
+
+  static endpoint() {
+    return 'api/products/'
+  }
+
+  static fromObject(obj){
+    const products = Product.fromObjects(obj.products)
+    return new Shop(obj.id, obj.name, obj.weight, products)
+  }
+
+}
