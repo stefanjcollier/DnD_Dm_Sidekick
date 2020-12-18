e088d2aef3ce06f05ad47903dc676ade86af4ade
Stefan Collier <stefanjcollier@hotmail.com>
2020-12-18 01:01:12 +0000

    stop annoying infinite redirect

--- pages/_error.js
+++ pages/_error.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import Router from "next/router";
 
 export default class _error extends Component {
-  componentDidMount = () => {
-    Router.push("/admin/dashboard");
-  };
+  // componentDidMount = () => {
+  //   Router.push("");
+  // };
 
   render() {
-    return <div />;
+    return <div>
+      <h1>Something went wrong</h1>
+      <p>Try going back to the main page</p>
+    </div>;
   }
 }
